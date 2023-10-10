27b0fe79cd438744624c88ec61623357fa302553
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2023-10-09 20:43:50 -0500

    Update GraphObject.cs

--- MetaMorpheus/EngineLayer/CombinatorialSearch/GraphObject.cs
+++ MetaMorpheus/EngineLayer/CombinatorialSearch/GraphObject.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Dynamic;
-using System.Linq;
-using System.Reflection;
-using MassSpectrometry;
+﻿using MassSpectrometry;
 using Proteomics;
-using Proteomics.AminoAcidPolymer;
 using Proteomics.Fragmentation;
 using Proteomics.ProteolyticDigestion;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
 
 namespace EngineLayer.CombinatorialSearch
 {
@@ -19,30 +17,29 @@ namespace EngineLayer.CombinatorialSearch
         public ImmutableList<Node> Nodes { get; set; }
         public PeptideSpectralMatch PSM { get; set; }
         public PeptideWithSetModifications Peptide { get; set; }
-        public List<Product> Products { get; set; }
         public List<Product> NterminalProducts { get; set; }
         public List<Product> CterminalProducts { get; set; }
         public string BaseSequence { get; set; }
         public Dictionary<int, Modification> Modifications { get; set; }
-        public int CterminusIndex { get; set; }
-        public int NterminusIndex { get; set; }
-        public double Coverage { get; set; }
+        public int NetworkCterminusIndex { get; set; }
+        public int NetworkNterminusIndex { get; set; }
+        public double NetworkCoverage { get; set; }
         public List<List<Node>> ConnectedNodes { get; set; } //new list for each fragment
 
-        public GraphObject(PeptideSpectralMatch psm)
+        public GraphObject(PeptideSpectralMatch psm) //todo pass common parameters to feed the experiment specifics such as DissociationType
         {
             PSM = psm;
             var nodes = new List<Node>();
 
             for (int i = 0; i < psm.BaseSequence.Length; i++)
             {
-                nodes.Add(new Node(psm.BaseSequence[i], i+1,
-                    psm.BaseSequence.Length-(i)));
+                nodes.Add(new Node(psm.BaseSequence[i], i + 1,
+                    psm.BaseSequence.Length - (i)));
             }
 
             Nodes = nodes.ToImmutableList();
+            // psm.BestMatchingPeptides.First().Peptide.AllModsOneIsNterminus.Clear(); //no mods
             Peptide = psm.BestMatchingPeptides.First().Peptide;
-            Products = new List<Product>();
             Modifications = Peptide.AllModsOneIsNterminus;
 
             //Sort Products into separate lists
@@ -51,8 +48,93 @@ namespace EngineLayer.CombinatorialSearch
             FragmentAndSortNodeProducts();
             SetMods();
 
-            CterminusIndex = 0;
-            NterminusIndex = Nodes.Count;
+            NetworkNterminusIndex = 0;
+            NetworkCterminusIndex = Nodes.Count;
+            NetworkCoverage = 0;
+
+            MatchSpectraAndScore();
+            FixMissedCleavageOnTerminal();
+            CheckEdgeNodes();
+        }
+
+        private void SetModificationDatabase()
+        {
+
+        }
+
+        private void MatchSpectraAndScore()
+        {
+            //Nterminal
+            var NterminalMatch = MetaMorpheusEngine.MatchFragmentIons(
+                new Ms2ScanWithSpecificMass(PSM.MsDataScan,
+                    PSM.ScanPrecursorMonoisotopicPeakMz,
+                    PSM.ScanPrecursorCharge, PSM.FullFilePath, new CommonParameters()),
+                NterminalProducts,
+                new CommonParameters(), true);
+
+            //Cterminal
+            CterminalProducts.Reverse(); //reverts the products order to begin with 1
+            var CterminalMatch = MetaMorpheusEngine.MatchFragmentIons(
+                new Ms2ScanWithSpecificMass(PSM.MsDataScan,
+                    PSM.ScanPrecursorMonoisotopicPeakMz,
+                    PSM.ScanPrecursorCharge, PSM.FullFilePath, new CommonParameters()),
+                CterminalProducts,
+                new CommonParameters(), true);
+
+            //Update matched status and sort MatchedFragmentIon to each Node
+            foreach (var node in Nodes)
+            {
+                node.NterminusFragmentIon = new List<MatchedFragmentIon>();
+                foreach (var fragment in NterminalMatch)
+                {
+                    if (node.NterminusIndex.Equals(fragment.NeutralTheoreticalProduct.FragmentNumber))
+                    {
+                        node.NterminusFragmentIon.Add(fragment);
+                        node.NterminusMatched = true;
+                        break;
+                    }
+                }
+            }
+
+            foreach (var node in Nodes)
+            {
+                node.CterminusFragmentIon = new List<MatchedFragmentIon>();
+                foreach (var fragment in CterminalMatch)
+                {
+                    if (node.CterminusIndex.Equals(fragment.NeutralTheoreticalProduct.FragmentNumber))
+                    {
+                        node.CterminusFragmentIon.Add(fragment);
+                        node.CterminusMatched = true;
+                        break;
+                    }
+                }
+            }
+
+            //Update Coverage todo
+            NetworkCoverage = 0; //reset to 0
+
+            foreach (var node in Nodes)
+            {
+                if (node.CterminusMatched)
+                    NetworkCoverage = NetworkCoverage + 1;
+                if(node.NterminusMatched)
+                    NetworkCoverage = NetworkCoverage + 1;
+            }
+
+            NetworkCoverage = NetworkCoverage / (NterminalProducts.Count + CterminalProducts.Count);
+        }
+
+        private void FixMissedCleavageOnTerminal()
+        {
+            if (Nodes.Last().CterminusTheoreticalProduct.Annotation.Equals("a0"))
+            {
+                foreach (var node in Nodes)
+                {
+                    node.CterminusIndex = node.CterminusIndex - 1;
+                }
+                MatchSpectraAndScore();
+            }
+
         }
 
         private void FragmentAndSortNodeProducts()
@@ -65,19 +147,10 @@ namespace EngineLayer.CombinatorialSearch
                 Nodes[i].NterminusTheoreticalProduct = NterminalProducts[i];
             }
 
-            if (Nodes.Last().Residue.Equals('L') || Nodes.Last().Residue.Equals('L'))
+            CterminalProducts.Reverse();
+            for (int i = 0; i < CterminalProducts.Count; i++)
             {
-                for (int i = CterminalProducts.Count - 1; i > 1; i--)
-                {
-                    Nodes[i].CterminusTheoreticalProduct = CterminalProducts[i];
-                }
-            }
-            else
-            {
-                for (int i = CterminalProducts.Count; i > 1; i--)
-                {
-                    Nodes[i].CterminusTheoreticalProduct = CterminalProducts[i];
-                }
+                Nodes[i].CterminusTheoreticalProduct = CterminalProducts[i];
             }
         }
 
@@ -90,14 +163,33 @@ namespace EngineLayer.CombinatorialSearch
         }
         public void MoveInwards()
         {
-            CterminusIndex = CterminusIndex - 1;
-            NterminusIndex = NterminusIndex + 1;
+            NetworkCterminusIndex = NetworkCterminusIndex - 1;
+            NetworkNterminusIndex = NetworkNterminusIndex + 1;
         }
 
-        public bool CheckEdgeNodes()
+        public bool CheckEdgeNodes() //todo fix for annotation check
         {
-            if (Nodes[CterminusIndex].Matched && Nodes[NterminusIndex].Matched)
+            if ((Nodes[NetworkNterminusIndex].NterminusMatched ||
+                 Nodes[NetworkNterminusIndex].NterminusFragmentIon.Equals("a0")) &&
+                (Nodes[NetworkNterminusIndex].CterminusMatched ||
+                 Nodes[NetworkNterminusIndex].CterminusFragmentIon.Equals("a0")) &&
+                (Nodes[NetworkCterminusIndex].NterminusMatched ||
+                 Nodes[NetworkCterminusIndex].NterminusFragmentIon.Equals("a0")) &&
+                (Nodes[NetworkCterminusIndex].CterminusMatched ||
+                 Nodes[NetworkCterminusIndex].CterminusFragmentIon.Equals("a0")))
+
+            {
+                Debug.WriteLine("Edges matched, moving inward");
                 return true;
+
+            }
+
+            if (Nodes[NetworkCterminusIndex - 1].CterminusTheoreticalProduct.NeutralMass >
+                PSM.MsDataScan.MassSpectrum.LastX)
+            {
+                Debug.WriteLine("mz over limit, moving inward");
+                return true;
+            }
 
             return false;
         }
@@ -113,16 +205,18 @@ namespace EngineLayer.CombinatorialSearch
         public int CterminusIndex { get; set; }
         public Product CterminusTheoreticalProduct { get; set; }
         public Product NterminusTheoreticalProduct { get; set; }
-        public Product CterminusExperimentalProduct { get; set; }
-        public Product? NterminusExperimentalProduct { get; set; }
+        public List<MatchedFragmentIon> NterminusFragmentIon { get; set; }
+        public List<MatchedFragmentIon> CterminusFragmentIon { get; set; }
         public Modification Modification { get; set; }
         public double? EvidencedMZ { get; set; }
-        public bool Matched { get; set; }
+        public bool NterminusMatched { get; set; }
+        public bool CterminusMatched { get; set; }
         public bool DatabaseEvidence { get; set; }
         public Node(char residue, int nterminus, int cterminus)
         {
             Residue = residue;
-            Matched = false;
+            NterminusMatched = false;
+            CterminusMatched = false;
             DatabaseEvidence = false;
             NterminusIndex = nterminus;
             CterminusIndex = cterminus;
